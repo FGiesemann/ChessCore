20d2d7c45b28520de8f9a2745321c68112ebe533
Florian Giesemann <florian.giesemann@gmail.com>
2025-06-19 22:01:23 +0200

    FEN checks also extract information
    The FenString class not only checks the FEN string for validity, but
    also extracts information like who is to move, the en passant target
    square, and move numbers.
    Not all information is extracted (e.g., the piece placement and castling
    rights are missing). And the validity check for the en passant target
    square is not yet dependent on the player to move next.

--- include/chesscore/fen.h
+++ include/chesscore/fen.h
@@ -7,6 +7,10 @@
 #define CHESSCORE_FEN_H
 
 #include "chesscore/chesscore.h"
+#include "chesscore/piece.h"
+#include "chesscore/square.h"
+
+#include <optional>
 #include <string>
 
 namespace chesscore {
@@ -23,20 +27,32 @@ class FenString {
 public:
     FenString();
     explicit FenString(const std::string &fen_string);
-    static auto startingPosition() -> FenString;
-    auto str() const -> std::string { return fen_string; }
+    static auto starting_position() -> FenString;
+    auto str() const -> std::string { return m_fen_string; }
+
+    auto side_to_move() const -> Color { return m_side_to_move; }
+    auto en_passant_square() const -> std::optional<Square> { return m_en_passant; }
+    auto halfmove_clock() const -> size_t { return m_halfmove_clock; }
+    auto fullmove_number() const -> size_t { return m_fullmove_number; }
 private:
-    std::string fen_string;
+    std::string m_fen_string;
+
+    Color m_side_to_move;
+    std::optional<Square> m_en_passant;
+    int m_halfmove_clock;
+    int m_fullmove_number;
 };
 
 namespace detail {
 
-auto check_piece_placement(const std::string &fen_string) -> size_t;
-auto check_side_to_move(const std::string &fen_string, size_t pos) -> size_t;
-auto check_castling_availability(const std::string &fen_string, size_t pos) -> size_t;
-auto check_en_passant_target_square(const std::string &fen_string, size_t pos) -> size_t;
-auto check_halfmove_clock(const std::string &fen_string, size_t pos) -> size_t;
-auto check_fullmove_number(const std::string &fen_string, size_t pos) -> void;
+auto check_piece_placement(const std::string &fen_string) -> std::size_t;
+auto check_side_to_move(const std::string &fen_string, std::size_t pos) -> std::pair<Color, std::size_t>;
+// TODO: extract castling availability
+auto check_castling_availability(const std::string &fen_string, std::size_t pos) -> std::size_t;
+// TODO: check dependent on side_to_move
+auto check_en_passant_target_square(const std::string &fen_string, std::size_t pos) -> std::pair<std::optional<Square>, std::size_t>;
+auto check_halfmove_clock(const std::string &fen_string, std::size_t pos) -> std::pair<int, std::size_t>;
+auto check_fullmove_number(const std::string &fen_string, std::size_t pos) -> int;
 
 } // namespace detail
 
