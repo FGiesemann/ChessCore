4fcc610c9d66dec7af309764b44005d56125b71f
Florian Giesemann <fgiesemann@proton.me>
2025-06-20 17:41:17 +0200

    Add API documentation

--- include/chesscore/chesscore.h
+++ include/chesscore/chesscore.h
@@ -11,16 +11,38 @@
 
 namespace chesscore {
 
+/**
+ * \brief A general exception thrown by the chess core.
+ */
 class ChessException : public std::exception {
 public:
+    /**
+     * \brief Construct a new exception object.
+     *
+     * \param message A message describing the error.
+     */
     ChessException(const std::string &message) : message{message} {};
+
+    /**
+     * \brief The message stored in the exception.
+     *
+     * \return The stored message.
+     */
     const char *what() const noexcept override { return message.c_str(); }
 private:
     std::string message;
 };
 
+/**
+ * \brief Exception thrown when an index is out of range.
+ */
 class OutOfRange : public ChessException {
 public:
+    /**
+     * \brief Construct a new exception.
+     *
+     * \param message Description of the error.
+     */
     OutOfRange(const std::string &message) : ChessException{message} {}
 };
 
