48c8e82cf0e32850fbe609d72137a199d09161ae
Florian Giesemann <florian.giesemann@gmail.com>
2025-06-22 20:12:17 +0200

    Add basic board and position class structure

--- include/chesscore/board.h
+++ include/chesscore/board.h
@@ -7,13 +7,26 @@
 #define CHESSCORE_BOARD_H
 
 #include "chesscore/chesscore.h"
+#include "chesscore/fen.h"
+#include "chesscore/piece.h"
+#include "chesscore/square.h"
 
 namespace chesscore {
 
 /**
- * \brief Representation of a chessboard.
+ * \brief A board describes the placement of pieces on a chess board.
+ *
  */
-class Board {};
+template<typename T>
+concept Board = requires(T board, Square s, Piece p, /* const Move &m,*/ const FenString &fen) {
+    { board.set_from_fen(fen) } -> std::same_as<void>;
+    //{ board.makeMove(m) } -> std::same_as<void>;
+    //{ board.unmakeMove(m) } -> std::same_as<void>;
+    { board.getPieceOnSquare(s) } -> std::same_as<Piece>;
+    { board.setPieceOnSquare(s, p) } -> std::same_as<void>;
+    { board.clearSquare(s) } -> std::same_as<void>;
+    { board.calculateZobristHashComponent() } -> std::same_as<uint64_t>;
+};
 
 } // namespace chesscore
 
