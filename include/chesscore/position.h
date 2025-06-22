32e4fcd055ca0b11bfe37115ecfebc1dff21abcf
Florian Giesemann <fgiesemann@proton.me>
2025-06-22 20:12:17 +0200

    Add basic board and position class structure

---
+++ include/chesscore/position.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** *
+ * Chess Core                                                                 *
+ * Data structures and algorithms for chess                                   *
+ * ************************************************************************** */
+
+#ifndef CHESSCORE_POSITION_H
+#define CHESSCORE_POSITION_H
+
+#include "chesscore/board.h"
+
+namespace chesscore {
+
+/**
+ * \brief The current state of a chess game.
+ *
+ * Represents the state of a game of chess. It includes the current placement of
+ * pieces on the board, the current turn, castling rights, the playerr to move,
+ * and many other pieces of information that describe the state of the game.
+ * \tparam BoardT The board representation that manages the pieces on the board.
+ */
+template<Board BoardT>
+class Position {};
+
+} // namespace chesscore
+
+#endif
