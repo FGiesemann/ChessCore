7ceae2203c63cce2403b74b29fad7ed2838afee6
Florian Giesemann <florian.giesemann@gmail.com>
2025-06-24 20:01:40 +0200

    Add get_piece to bitboard

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -12,7 +12,7 @@ auto Bitboard::empty() const -> bool {
 }
 
 auto Bitboard::has_piece(const PieceType &piece_type) const -> bool {
-    return !(bitmap({piece_type, Color::White}) | bitmap({piece_type, Color::Black})).empty();
+    return !(bitmap({.type = piece_type, .color = Color::White}) | bitmap({.type = piece_type, .color = Color::Black})).empty();
 }
 
 auto Bitboard::has_piece(const Piece &piece) const -> bool {
@@ -29,7 +29,16 @@ auto Bitboard::set_piece(const Piece &piece, const Square &square) -> void {
     bitmap(piece.color).set(square);
 }
 
-auto Bitboard::get_piece([[maybe_unused]] const Square &square) const -> std::optional<Piece> {
+auto Bitboard::get_piece(const Square &square) const -> std::optional<Piece> {
+    if (!m_all_pieces.get(square)) {
+        return {};
+    }
+    const auto [min_index, max_index] = (m_white_pieces.get(square)) ? std::make_pair(min_white_piece_index, max_white_piece_index) : std::make_pair(min_black_piece_index, max_black_piece_index);
+    for (auto i = min_index; i <= max_index; ++i) {
+        if (m_bitmaps.at(i).get(square)) {
+            return Piece{.type = static_cast<PieceType>(i - min_index), .color = (i <= max_white_piece_index) ? Color::White : Color::Black};
+        }
+    }
     return {};
 }
 
