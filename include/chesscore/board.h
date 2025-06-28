47268b02d2909c9b7085bd220e4a976d3b20ec97
Florian Giesemann <florian.giesemann@gmail.com>
2025-06-28 16:55:45 +0200

    Add make_move and unmake_move to Board concept

--- include/chesscore/board.h
+++ include/chesscore/board.h
@@ -8,6 +8,7 @@
 #define CHESSCORE_BOARD_H
 
 #include "chesscore/chesscore.h"
+#include "chesscore/move.h"
 #include "chesscore/piece.h"
 
 namespace chesscore {
@@ -22,21 +23,22 @@ class FenString;
  *
  */
 template<typename T>
-concept Board = requires(T board, const T board_c, const Square &square, const Piece &piece, const PieceType &piece_type, const Color &color, /* const Move &move,*/ const FenString &fen) {
-    std::is_default_constructible_v<T>;
-    std::is_constructible_v<T, const FenString &>;
-    { board_c.empty() } -> std::same_as<bool>;
-    { board_c.has_piece(piece_type) } -> std::same_as<bool>;
-    { board_c.has_piece(piece) } -> std::same_as<bool>;
-    { board_c.has_piece(color) } -> std::same_as<bool>;
-    { board_c.has_piece(square) } -> std::same_as<bool>;
-    { board.set_piece(piece, square) } -> std::same_as<void>;
-    { board_c.get_piece(square) } -> std::same_as<std::optional<Piece>>;
-    { board.clear_square(square) } -> std::same_as<void>;
-    //{ board.make_move(move) } -> std::same_as<void>;
-    //{ board.unmake_move(move) } -> std::same_as<void>;
-    { board_c.calculate_hash_component() } -> std::same_as<uint64_t>; /* TODO: return type: Hash-Type */
-};
+concept Board =
+    requires(T board, const T board_c, const Square &square, const Piece &piece, const PieceType &piece_type, const Color &color, const Move &move, const FenString &fen) {
+        std::is_default_constructible_v<T>;
+        std::is_constructible_v<T, const FenString &>;
+        { board_c.empty() } -> std::same_as<bool>;
+        { board_c.has_piece(piece_type) } -> std::same_as<bool>;
+        { board_c.has_piece(piece) } -> std::same_as<bool>;
+        { board_c.has_piece(color) } -> std::same_as<bool>;
+        { board_c.has_piece(square) } -> std::same_as<bool>;
+        { board.set_piece(piece, square) } -> std::same_as<void>;
+        { board_c.get_piece(square) } -> std::same_as<std::optional<Piece>>;
+        { board.clear_square(square) } -> std::same_as<void>;
+        { board.make_move(move) } -> std::same_as<void>;
+        { board.unmake_move(move) } -> std::same_as<void>;
+        { board_c.calculate_hash_component() } -> std::same_as<uint64_t>; /* TODO: return type: Hash-Type */
+    };
 
 } // namespace chesscore
 
