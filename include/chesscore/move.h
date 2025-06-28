15c3ebab86c4176423873d2af6a75f097ab498d0
Florian Giesemann <florian.giesemann@gmail.com>
2025-06-28 16:38:40 +0200

    Create Move class

---
+++ include/chesscore/move.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** *
+ * Chess Core                                                                 *
+ * Data structures and algorithms for chess                                   *
+ * ************************************************************************** */
+/** \file */
+
+#ifndef CHESSCORE_MOVE_H
+#define CHESSCORE_MOVE_H
+
+#include "chesscore/chesscore.h"
+#include "chesscore/piece.h"
+#include "chesscore/square.h"
+
+namespace chesscore {
+
+struct Move {};
+
+} // namespace chesscore
+
+#endif
