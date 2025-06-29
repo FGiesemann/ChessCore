a860f7f2d98bb2af0e8a97d1c6a5f9f9d88432f3
Florian Giesemann <fgiesemann@proton.me>
2025-06-29 12:25:39 +0200

    Add comprehensive castling tests

--- test/bitboard/make_move_test.cpp
+++ test/bitboard/make_move_test.cpp
@@ -133,3 +133,76 @@ TEST_CASE("Bitboard.Bitboard.MakeMove.Promotion", "[Bitboard][MakeMove]") {
     CHECK_FALSE(board.get_piece(Square::F7).has_value());
     CHECK(board.get_piece(Square::F8) == Piece::WhiteQueen);
 }
+
+TEST_CASE("Bitboard.Bitboard.MakeMove.Castling", "[Bitboard][MakeMove]") {
+    FenString fen{"r3k2r/ppp2ppp/8/8/8/8/PPP2PPP/R3K2R w KQkq - 0 1"};
+    Bitboard board1{fen};
+    Bitboard board2{fen};
+    Bitboard board3{fen};
+    Bitboard board4{fen};
+
+    Move white_kingside_castling{
+        .from = Square::E1,
+        .to = Square::G1,
+        .piece = Piece::WhiteKing,
+        .captured{},
+        .promoted{},
+        .en_passant{},
+        .castling_rights_before{CastlingRights::all()},
+        .halfmove_clock_before = 0,
+    };
+    Move white_queenside_castling{
+        .from = Square::E1,
+        .to = Square::C1,
+        .piece = Piece::WhiteKing,
+        .captured{},
+        .promoted{},
+        .en_passant{},
+        .castling_rights_before{CastlingRights::all()},
+        .halfmove_clock_before = 0,
+    };
+    Move black_kingside_castling{
+        .from = Square::E8,
+        .to = Square::G8,
+        .piece = Piece::BlackKing,
+        .captured{},
+        .promoted{},
+        .en_passant{},
+        .castling_rights_before{CastlingRights::all()},
+        .halfmove_clock_before = 0,
+    };
+    Move black_queenside_castling{
+        .from = Square::E8,
+        .to = Square::C8,
+        .piece = Piece::BlackKing,
+        .captured{},
+        .promoted{},
+        .en_passant{},
+        .castling_rights_before{CastlingRights::all()},
+        .halfmove_clock_before = 0,
+    };
+
+    board1.make_move(white_kingside_castling);
+    CHECK_FALSE(board1.get_piece(Square::E1).has_value());
+    CHECK(board1.get_piece(Square::G1) == Piece::WhiteKing);
+    CHECK_FALSE(board1.get_piece(Square::H1).has_value());
+    CHECK(board1.get_piece(Square::F1) == Piece::WhiteRook);
+
+    board2.make_move(white_queenside_castling);
+    CHECK_FALSE(board2.get_piece(Square::E1).has_value());
+    CHECK(board2.get_piece(Square::C1) == Piece::WhiteKing);
+    CHECK_FALSE(board2.get_piece(Square::A1).has_value());
+    CHECK(board2.get_piece(Square::D1) == Piece::WhiteRook);
+
+    board3.make_move(black_kingside_castling);
+    CHECK_FALSE(board3.get_piece(Square::E8).has_value());
+    CHECK(board3.get_piece(Square::G8) == Piece::BlackKing);
+    CHECK_FALSE(board3.get_piece(Square::H8).has_value());
+    CHECK(board3.get_piece(Square::F8) == Piece::BlackRook);
+
+    board4.make_move(black_queenside_castling);
+    CHECK_FALSE(board4.get_piece(Square::E8).has_value());
+    CHECK(board4.get_piece(Square::C8) == Piece::BlackKing);
+    CHECK_FALSE(board4.get_piece(Square::A8).has_value());
+    CHECK(board4.get_piece(Square::D8) == Piece::BlackRook);
+}
