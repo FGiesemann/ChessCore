92267e8ccbd2ce0829cd1c5353dc934bdcc0e151
Florian Giesemann <fgiesemann@proton.me>
2025-07-03 18:41:15 +0200

    Add Pattern and shift method; Knight pattern as an example

--- tools/table_generator/bigmap.h
+++ tools/table_generator/bigmap.h
@@ -14,24 +14,26 @@ static constexpr std::size_t board_width{8};
 static constexpr std::size_t overhang{board_width - 1};
 static constexpr std::size_t bigmap_width{2 * overhang + board_width};
 static constexpr std::size_t a1_offset{overhang * bigmap_width};
+static constexpr std::size_t pattern_width{overhang + board_width};
+
+using Pattern = std::bitset<pattern_width * pattern_width>;
+
+extern const Pattern knight_pattern;
+
+enum class ShiftDirection { North, South, East, West };
 
 class Bigmap {
 public:
+    Bigmap() = default;
+    Bigmap(const Pattern &pattern);
+
     using Bitset = std::bitset<bigmap_width * bigmap_width>;
 
     auto operator[](std::size_t pos) const -> bool { return m_bits[pos]; }
     auto at(std::size_t row, std::size_t col) const -> bool { return m_bits[index(row, col)]; }
     auto operator[](std::size_t pos) -> Bitset::reference { return m_bits[pos]; }
 
-    auto operator<<=(std::size_t pos) -> Bigmap & {
-        m_bits <<= pos;
-        return *this;
-    }
-
-    auto operator>>=(std::size_t pos) -> Bigmap & {
-        m_bits >>= pos;
-        return *this;
-    }
+    auto shift(ShiftDirection dir) -> void;
 
     auto extract_board() const -> Bitmap;
     auto index(std::size_t row, std::size_t col) const -> std::size_t { return row * bigmap_width + col; }
