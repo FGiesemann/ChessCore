c11802608bfcae4ed17a69cdc26636645facdd8e
Florian Giesemann <florian.giesemann@gmail.com>
2025-07-04 17:29:07 +0200

    Finish table generator
    The table generator tool can now generate piece target tables for every
    piece type, except pawns.

--- tools/table_generator/main.cpp
+++ tools/table_generator/main.cpp
@@ -4,16 +4,32 @@
  * ************************************************************************** */
 
 #include <iostream>
+#include <unordered_map>
 
 #include "bigmap.h"
-#include "chesscore_io/bitmap_io.h"
+#include "table_gen.h"
 
-auto main() -> int {
+auto main(int argc, const char *argv[]) -> int {
     using namespace chesscore::table_gen;
-    Bigmap bigmap{knight_pattern};
 
-    std::cout << bigmap << '\n';
+    std::unordered_map<std::string, const Pattern &> patterns{
+        {"knight", knight_pattern},
+        {"bishop", bishop_pattern},
+        {"rook",   rook_pattern  },
+        {"queen",  queen_pattern },
+        {"king",   king_pattern  },
+    };
 
-    std::cout << bigmap.extract_board() << '\n';
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <piece>\n";
+        std::cerr << "  possible pieces:\n";
+        for (const auto &e : patterns) {
+            std::cerr << "    " << e.first << '\n';
+        }
+        return 1;
+    }
+
+    generate_table(argv[1], patterns.at(argv[1]), std::cout);
+
     return 0;
 }
