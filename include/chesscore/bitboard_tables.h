f9b5dfb76d9f7ca667edf499e5d4fd2f9150bfa7
Florian Giesemann <florian.giesemann@gmail.com>
2025-07-05 10:59:24 +0200

    Add PieceTargetTables

--- include/chesscore/bitboard_tables.h
+++ include/chesscore/bitboard_tables.h
@@ -8,6 +8,7 @@
 #define CHESSCORE_BITBOARD_TABLES_H
 
 #include "chesscore/bitmap.h"
+#include "chesscore/piece.h"
 #include "chesscore/square.h"
 #include "chesscore/table.h"
 
@@ -20,6 +21,10 @@ extern const TargetTable bishop_target_table;
 extern const TargetTable rook_target_table;
 extern const TargetTable queen_target_table;
 extern const TargetTable king_target_table;
+
+using PieceTargetTables = Table<const TargetTable *, piece_type_count, PieceType>;
+
+extern const PieceTargetTables piece_target_tables;
 
 } // namespace chesscore
 
