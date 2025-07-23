bce5ab40a25e7e88b8cc691e70ce4c07794197e7
Florian Giesemann <florian.giesemann@gmail.com>
2025-07-23 22:01:39 +0200

    Remove tools and integrate chesscore_io

---
+++ include/chesscore_io/chesscore_io.h
@@ -0,0 +1,14 @@
+/* ************************************************************************** *
+ * Chess Core                                                                 *
+ * Data structures and algorithms for chess                                   *
+ * ************************************************************************** */
+/** \file */
+
+#ifndef CHESSCORE_IO_CHESSCORE_IO_H
+#define CHESSCORE_IO_CHESSCORE_IO_H
+
+#include "chesscore_io/bitboard_io.h"
+#include "chesscore_io/bitmap_io.h"
+#include "chesscore_io/piece_io.h"
+
+#endif
