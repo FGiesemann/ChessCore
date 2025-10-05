7e94e8d3db2d644d0ec85d0f180ebaa3835b08dc
Florian Giesemann <fgiesemann@proton.me>
2025-10-05 11:17:59 +0200

    Add conversion from CheckState to symbol

--- src/chesscore/position_types.cpp
+++ src/chesscore/position_types.cpp
@@ -4,3 +4,17 @@
  * ************************************************************************** */
 
 #include "chesscore/position_types.h"
+
+namespace chesscore {
+
+auto check_state_symbol(CheckState state) -> std::string {
+    if (state == CheckState::Check) {
+        return "+";
+    }
+    if (state == CheckState::Checkmate) {
+        return "#";
+    }
+    return "";
+}
+
+}
