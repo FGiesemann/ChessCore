bcc8dc1dd41f85cefc04b9def71f6821a523fae7
Florian Giesemann <florian.giesemann@gmail.com>
2025-10-15 18:05:55 +0200

    Finish parsing single EPD record

--- include/chesscore/epd.h
+++ include/chesscore/epd.h
@@ -16,6 +16,20 @@
 namespace chesscore {
 
 /**
+ * \brief Exception thrown when an invalid EPD record is encountered.
+ *
+ */
+class InvalidEpd : public ChessException {
+public:
+    /**
+     * \brief Create an exeption with a message.
+     *
+     * \param message The message.
+     */
+    InvalidEpd(const std::string &message) : ChessException("Invalid EPD record: " + message) {}
+};
+
+/**
  * \brief An EPD record.
  *
  * A record describing a position in an EPD file.
@@ -29,33 +43,33 @@ struct EpdRecord {
     using move_list = std::vector<san_str>;                        ///< A (possibly empty) list of moves.
     using player_identifier = std::pair<std::string, std::string>; ///< Identifier (email and name) of a player.
 
-    Position position;      ///< The position described by the record.
-    opt_int acd;            ///< analysis count: depth.
-    opt_int acn;            ///< analysis count: nodes.
-    opt_int acs;            ///< analysis count: seconds.
-    move_list bm;           ///< best moves.
-    str_list c;             ///< comments.
-    opt_int ce;             ///< centipawn evaluation.
-    opt_int dm;             ///< direct mate.
-    bool draw_accept;       ///< draw accept.
-    bool draw_claim;        ///< draw claim.
-    bool draw_offer;        ///< draw offer.
-    bool draw_reject;       ///< draw reject.
-    opt_str eco;            ///< ECO code.
-    opt_int fmvn;           ///< fullmove number.
-    opt_int hmvc;           ///< halfmove clock.
-    opt_str id;             ///< id.
-    opt_str nic;            ///< New In Chess opening code.
-    str_list noop_ops;      ///< noop operands.
-    opt_move pm;            ///< predicted move.
-    move_list pv;           ///< predicted variations.
-    opt_int rc;             ///< repetition count.
-    bool resign;            ///< resign.
-    opt_move sm;            ///< supplied move.
-    opt_int tcgs;           ///< telecommunication: game selector.
-    player_identifier tcri; ///< telecommunication: receiver identification.
-    player_identifier tcsi; ///< telecommunication: sender identification.
-    str_list v;             ///< variation names.
+    Position position;         ///< The position described by the record.
+    opt_int acd;               ///< analysis count: depth.
+    opt_int acn;               ///< analysis count: nodes.
+    opt_int acs;               ///< analysis count: seconds.
+    move_list bm;              ///< best moves.
+    str_list c = str_list(10); ///< comments.
+    opt_int ce;                ///< centipawn evaluation.
+    opt_int dm;                ///< direct mate.
+    bool draw_accept;          ///< draw accept.
+    bool draw_claim;           ///< draw claim.
+    bool draw_offer;           ///< draw offer.
+    bool draw_reject;          ///< draw reject.
+    opt_str eco;               ///< ECO code.
+    opt_int fmvn;              ///< fullmove number.
+    opt_int hmvc;              ///< halfmove clock.
+    opt_str id;                ///< id.
+    opt_str nic;               ///< New In Chess opening code.
+    str_list noop_ops;         ///< noop operands.
+    opt_move pm;               ///< predicted move.
+    move_list pv;              ///< predicted variations.
+    opt_int rc;                ///< repetition count.
+    bool resign;               ///< resign.
+    opt_move sm;               ///< supplied move.
+    opt_int tcgs;              ///< telecommunication: game selector.
+    player_identifier tcri;    ///< telecommunication: receiver identification.
+    player_identifier tcsi;    ///< telecommunication: sender identification.
+    str_list v = str_list(10); ///< variation names.
 };
 
 auto parse_epd_line(const std::string &line) -> EpdRecord;
--- src/chesscore/epd.cpp
+++ src/chesscore/epd.cpp
@@ -22,12 +22,183 @@ auto read_fen_fields(const std::string &line) -> std::pair<FenString, size_t> {
     return std::make_pair(fen, en_p.second);
 }
 
+auto advance(const std::string &line, size_t &index) {
+    while (index < line.length() && line[index] == ' ') {
+        ++index;
+    }
+}
+
+auto read_word(const std::string &line, size_t &index) -> std::string {
+    advance(line, index);
+    const auto start = index;
+    while (index < line.length() && line[index] != ' ' && line[index] != ';') {
+        ++index;
+    }
+    return line.substr(start, index - start);
+}
+
+auto read_int(const std::string &line, size_t &index, EpdRecord::opt_int &opt_int) -> void {
+    advance(line, index);
+    const auto start = index;
+    while (index < line.length() && std::isdigit(line[index]) != 0) {
+        ++index;
+    }
+    opt_int = std::stoi(line.substr(start, index - start));
+}
+
+auto read_move(const std::string &line, size_t &index, EpdRecord::opt_move &opt_move) -> void {
+    advance(line, index);
+    opt_move = read_word(line, index);
+}
+
+auto read_moves(const std::string &line, size_t &index, EpdRecord::move_list &moves) -> void {
+    advance(line, index);
+    while (index < line.length() && line[index] != ';') {
+        moves.emplace_back(read_word(line, index));
+        advance(line, index);
+    }
+    ++index;
+}
+
+auto check_operation_end(const std::string &line, size_t &index) -> void {
+    advance(line, index);
+    if (index >= line.length() || line[index] != ';') {
+        throw InvalidEpd("Invalid operation end");
+    }
+    ++index;
+}
+
+auto get_string(const std::string &line, size_t &index) -> std::string {
+    advance(line, index);
+    if (index >= line.length() || line[index] != '"') {
+        throw InvalidEpd("Invalid string");
+    }
+    ++index;
+    const auto start = index;
+    while (index < line.length() && line[index] != '"') {
+        ++index;
+    }
+    return line.substr(start, index++ - start);
+}
+
+auto read_string(const std::string &line, size_t &index, EpdRecord::str_list &list, int list_index) -> void {
+    list[list_index] = get_string(line, index);
+}
+
+auto read_string(const std::string &line, size_t &index, EpdRecord::opt_str &opt_str) -> void {
+    opt_str = get_string(line, index);
+}
+
+auto collect_operands(const std::string &line, size_t &index, EpdRecord::str_list &list) -> void {
+    advance(line, index);
+    while (index < line.length() && line[index] != ';') {
+        const auto operand = read_word(line, index);
+        list.emplace_back(operand);
+        advance(line, index);
+    }
+    ++index;
+}
+
+auto read_player_identifier(const std::string &line, size_t &index, EpdRecord::player_identifier &identifier) -> void {
+    identifier.first = read_word(line, index);
+    identifier.second = get_string(line, index);
+}
+
+auto read_operation(const std::string &line, size_t &index, EpdRecord &record) -> void {
+    const auto opcode = read_word(line, index);
+    if (opcode == "acd") {
+        read_int(line, index, record.acd);
+        check_operation_end(line, index);
+    } else if (opcode == "acn") {
+        read_int(line, index, record.acn);
+        check_operation_end(line, index);
+    } else if (opcode == "acs") {
+        read_int(line, index, record.acs);
+        check_operation_end(line, index);
+    } else if (opcode == "bm") {
+        read_moves(line, index, record.bm);
+    } else if (opcode == "c0" || opcode == "c1" || opcode == "c2" || opcode == "c3" || opcode == "c4" || opcode == "c5" || opcode == "c6" || opcode == "c7" || opcode == "c8" ||
+               opcode == "c9") {
+        read_string(line, index, record.c, opcode[1] - '0');
+        check_operation_end(line, index);
+    } else if (opcode == "ce") {
+        read_int(line, index, record.ce);
+        check_operation_end(line, index);
+    } else if (opcode == "dm") {
+        read_int(line, index, record.dm);
+        check_operation_end(line, index);
+    } else if (opcode == "draw_accept;") {
+        record.draw_accept = true;
+        ++index;
+    } else if (opcode == "draw_claim;") {
+        record.draw_claim = true;
+        ++index;
+    } else if (opcode == "draw_offer;") {
+        record.draw_offer = true;
+        ++index;
+    } else if (opcode == "draw_reject;") {
+        record.draw_reject = true;
+        ++index;
+    } else if (opcode == "eco") {
+        read_string(line, index, record.eco);
+        check_operation_end(line, index);
+    } else if (opcode == "fmvn") {
+        read_int(line, index, record.fmvn);
+        check_operation_end(line, index);
+    } else if (opcode == "hmvc") {
+        read_int(line, index, record.hmvc);
+        check_operation_end(line, index);
+    } else if (opcode == "id") {
+        read_string(line, index, record.id);
+        check_operation_end(line, index);
+    } else if (opcode == "nic") {
+        read_string(line, index, record.nic);
+        check_operation_end(line, index);
+    } else if (opcode == "noop") {
+        collect_operands(line, index, record.noop_ops);
+    } else if (opcode == "pm") {
+        read_move(line, index, record.pm);
+        check_operation_end(line, index);
+    } else if (opcode == "pv") {
+        read_moves(line, index, record.pv);
+    } else if (opcode == "rc") {
+        read_int(line, index, record.rc);
+        check_operation_end(line, index);
+    } else if (opcode == "resign;") {
+        record.resign = true;
+        ++index;
+    } else if (opcode == "sm") {
+        read_move(line, index, record.sm);
+        check_operation_end(line, index);
+    } else if (opcode == "tcgs") {
+        read_int(line, index, record.tcgs);
+        check_operation_end(line, index);
+    } else if (opcode == "tcri") {
+        read_player_identifier(line, index, record.tcri);
+        check_operation_end(line, index);
+    } else if (opcode == "tcsi") {
+        read_player_identifier(line, index, record.tcsi);
+        check_operation_end(line, index);
+    } else if (opcode == "v0" || opcode == "v1" || opcode == "v2" || opcode == "v3" || opcode == "v4" || opcode == "v5" || opcode == "v6" || opcode == "v7" || opcode == "v8" ||
+               opcode == "v9") {
+        read_string(line, index, record.v, opcode[1] - '0');
+        check_operation_end(line, index);
+    }
+}
+
 } // namespace
 
 auto parse_epd_line(const std::string &line) -> EpdRecord {
-    const auto [fen, index] = read_fen_fields(line);
+    auto [fen, index] = read_fen_fields(line);
+    if (index < line.length()) {
+        advance(line, index);
+    }
     EpdRecord record;
     record.position = Position{fen};
+    while (index < line.length()) {
+        read_operation(line, index, record);
+    }
+
     return record;
 }
 
