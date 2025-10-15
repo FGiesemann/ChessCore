a05a75e1b1c27c82812d765d8bd367d515051911
Florian Giesemann <fgiesemann@proton.me>
2025-10-15 18:05:55 +0200

    Finish parsing single EPD record

--- test/data/epd_test.cpp
+++ test/data/epd_test.cpp
@@ -58,3 +58,56 @@ TEST_CASE("Data.EPD.Parse.Positions.Position 3", "[EPD]") {
     CHECK(position.halfmove_clock() == 0);
     CHECK(position.fullmove_number() == 1);
 }
+
+TEST_CASE("Data.EPD.Parse.Operations.Best Move", "[EPD]") {
+    const auto record1 = parse_epd_line("8/3r4/pr1Pk1p1/8/7P/6P1/3R3K/5R2 w - - bm Re2+;");
+    const auto &position1 = record1.position;
+
+    CHECK(position1.piece_placement() == placement_from_string("_____R_____R___K______P________P________pr_Pk_p____r____________"));
+    CHECK(position1.side_to_move() == Color::White);
+    CHECK(position1.castling_rights() == CastlingRights::none());
+    CHECK_FALSE(position1.en_passant_target().has_value());
+    CHECK(position1.halfmove_clock() == 0);
+    CHECK(position1.fullmove_number() == 1);
+    CHECK(record1.bm == EpdRecord::move_list{"Re2+"});
+
+    const auto record2 = parse_epd_line("r1b2rk1/2q1b1pp/p2ppn2/1p6/3QP3/1BN1B3/PPP3PP/R4RK1 w - - bm Nd5 a4;");
+    const auto position2 = record2.position;
+
+    CHECK(position2.piece_placement() == placement_from_string("R____RK_PPP___PP_BN_B______QP____p______p__ppn____q_b_ppr_b__rk_"));
+    CHECK(position2.side_to_move() == Color::White);
+    CHECK(position1.castling_rights() == CastlingRights::none());
+    CHECK_FALSE(position2.en_passant_target().has_value());
+    CHECK(position2.halfmove_clock() == 0);
+    CHECK(position2.fullmove_number() == 1);
+    CHECK(record2.bm == EpdRecord::move_list{"Nd5", "a4"});
+}
+
+TEST_CASE("Data.EPD.Parse.Operations.Multiple", "[EPD]") {
+    const auto record =
+        parse_epd_line(R"(r3k2r/pp4p1/2p1pnp1/3p1pn1/2PP4/1P2qPPP/P1Q3BK/3RBR2 b kq - bm Nxh3; id "arasan2024.200"; c0 "BOT ArasanX - BOT MateAI, lichess.org 2023";)");
+
+    CHECK(record.bm == EpdRecord::move_list{"Nxh3"});
+    CHECK(record.id == "arasan2024.200");
+    REQUIRE(record.c.size() == 10);
+    CHECK(record.c[0].has_value());
+    CHECK(record.c[0] == "BOT ArasanX - BOT MateAI, lichess.org 2023");
+}
+
+TEST_CASE("Data.EPD.Parse.Operations.Operands", "[EPD]") {
+    const auto record = parse_epd_line(
+        R"(7b/8/kq6/8/8/1N2R3/K2P4/8 w - - bm a4; id "name"; c0 "comment0"; c1 "comment1"; acd 3; noop opA opB 123 "test" xyz; tcri player@server.de "Player Name";)"
+    );
+
+    CHECK(record.bm == EpdRecord::move_list{"a4"});
+    CHECK(record.id == "name");
+    CHECK(record.c[0].has_value());
+    CHECK(record.c[0] == "comment0");
+    CHECK(record.c[1].has_value());
+    CHECK(record.c[1] == "comment1");
+    CHECK(record.acd == 3);
+    CHECK(record.noop_ops.size() == 5);
+    CHECK(record.noop_ops == EpdRecord::str_list{"opA", "opB", "123", "\"test\"", "xyz"});
+    CHECK(record.tcri.first == "player@server.de");
+    CHECK(record.tcri.second == "Player Name");
+}
