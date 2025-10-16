5c5635d84067a96f2b669b10513437f221d1d460
Florian Giesemann <florian.giesemann@gmail.com>
2025-10-16 16:53:09 +0200

    Remove unnecessary test in if

--- src/chesscore/piece.cpp
+++ src/chesscore/piece.cpp
@@ -10,7 +10,7 @@
 namespace chesscore {
 
 auto piece_type_from_index(std::size_t index) -> PieceType {
-    if (index >= 0 && index < piece_type_count) {
+    if (index < piece_type_count) {
         return all_piece_types[index];
     }
     throw ChessException("Invalid piece type index: " + std::to_string(index));
