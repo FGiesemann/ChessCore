ce7b648a1b1be88e42bfdd7b219c47ec7c0bb49c
Florian Giesemann <fgiesemann@proton.me>
2026-07-12 11:59:23 +0200

    Add test involving "sign bits"

--- test/magic/blocker_iteration_test.cpp
+++ test/magic/blocker_iteration_test.cpp
@@ -50,3 +50,18 @@ TEST_CASE("Magic.Blocker.Iteration.Spread Bits", "[magic]") {
         CHECK(blockers == expected_blocker);
     }
 }
+
+TEST_CASE("Magic.Blocker.Iteration.Sign Bit", "[magic]") {
+    const Bitmap mask{0xF000000000000000ULL};
+
+    const std::array<Bitmap, 16> expected = {Bitmap{0x1000000000000000ULL}, Bitmap{0x2000000000000000ULL}, Bitmap{0x3000000000000000ULL}, Bitmap{0x4000000000000000ULL},
+                                             Bitmap{0x5000000000000000ULL}, Bitmap{0x6000000000000000ULL}, Bitmap{0x7000000000000000ULL}, Bitmap{0x8000000000000000ULL},
+                                             Bitmap{0x9000000000000000ULL}, Bitmap{0xA000000000000000ULL}, Bitmap{0xB000000000000000ULL}, Bitmap{0xC000000000000000ULL},
+                                             Bitmap{0xD000000000000000ULL}, Bitmap{0xE000000000000000ULL}, Bitmap{0xF000000000000000ULL}, Bitmap{0x0000000000000000ULL}};
+
+    Bitmap blockers;
+    for (const auto &expected_blocker : expected) {
+        blockers = next_blocker_config(blockers, mask);
+        CHECK(blockers == expected_blocker);
+    };
+}
